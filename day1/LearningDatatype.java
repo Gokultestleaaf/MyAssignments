305ebed4be6199d6f21615bacd7252cccefcd977
Gokultestleaaf <gokulnath.r1994@gmail.com>
2025-03-02 11:49:23 +0530

    Add files via upload
    Day1 uploaded

---
+++ day1/LearningDatatype.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+class LearningDatatype {
+
+  public static void main(String[] args) {
+	
+	int regno=435029;
+	
+	boolean isacar=true;
+	
+	float speed=0.10134578232f;
+	
+	char logo='T';
+	
+	String name="Benz";
+	
+	long phno=9840829255l;
+	
+	double capacity=123.12345678123;
+	
+	System.out.println("regno is:"+regno);
+	
+	System.out.println("logo:"+logo);
+	
+	System.out.println("name of the car is:"+name);
+	
+	System.out.println("phno is:"+phno);
+	
+	System.out.println("capacity is:"+capacity);
+}
+
+}
